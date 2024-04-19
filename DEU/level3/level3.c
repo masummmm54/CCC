8dfa581f1afa727380b8599ef81997ce908aa90e
Muhammet Kose <muhakose@3-C-7.42heilbronn.de>
2024-04-19 18:51:42 +0200

    ads

--- DEU/level3/level3.c
+++ DEU/level3/level3.c
@@ -58,6 +58,13 @@ void width(char *s)
 char **dup_map(char **map, int h, int w)
 {
 
+	int m = 0;
+	//if (map != NULL)
+	//{
+	//	while (m < h)
+	//		free(map[m++]);
+	//	free(map);
+	//}
 	char **dup_map = malloc(sizeof(char *) * h + 1);
 	int i = 0;
 	int j = 0;
@@ -68,8 +75,11 @@ char **dup_map(char **map, int h, int w)
 		while (j < w)
 		{
 			if (map[i][j] == 'X')
-				dup_map[i][j] = '1';
-			dup_map[i][j] = '0';
+			{
+				dup_map[i][j] = 'X';
+			}
+			else
+				dup_map[i][j] = '0';
 			j++;
 		}
 		i++;
@@ -79,7 +89,19 @@ char **dup_map(char **map, int h, int w)
 	return (dup_map);
 }
 
-// ASSDWDSDWWDSS x = 1 y = 0
+int is_visited(char **map, int h, int w)
+{
+	for(int i = 0; i < h; i++)
+	{
+		for (int j = 0; j < w; j++)
+		{
+			if (map[i][j] == '0' && map[i][j] != 'X')
+				return (1);
+		}
+	}
+	return (0);
+}
+
 int	move_pos(char **map, char *moves, int x, int y, int h, int w)
 {
 	for (int i = 0; moves[i + 1]; i++)
@@ -87,11 +109,11 @@ int	move_pos(char **map, char *moves, int x, int y, int h, int w)
 		map[y][x] = '1';
 		if (moves[i] == 'S')
 		{
-			y -= 1;
+			y += 1;
 		}
 		else if (moves[i] == 'W')
 		{
-			y += 1;
+			y -= 1;
 		}
 		else if (moves[i] == 'D')
 		{
@@ -103,25 +125,24 @@ int	move_pos(char **map, char *moves, int x, int y, int h, int w)
 		}
 		if (x >= w || y >= h || x < 0 || y < 0)
 			return (0);
-		if (map[y][x] == '1')
-		{
+		if (map[y][x] == '1' || map[y][x] == 'X')
 			return (0);
-		}
 	}
+	map[y][x] = '1';
+	//printf("x = %d\n", x);
+	//printf("y = %d\n", y);
+	//printf("w = %d\n", w);
+	//printf("h = %d\n", h);
+	if (!(x == 0 || x == w - 1 || y == 0 || y == h - 1))
+		return (0);
+
+	if (is_visited(map,h,w))
+		return (0);
 	return (1);
 }
 
 int is_valid(char **map, char *moves, int w, int h, int x, int y)
 {
-	//printf("x = %d\n", x);
-	//printf("y = %d\n", y);
-	//printf("w = %d\n", w);
-	//printf("h = %d\n", h);
-	//if (x >= w || y >= h || x < 0 || y < 0)
-	//	return (0);
-	//is_valid(map, moves, w, h, x + 1, y);
-	//is_valid(map, moves, w, h, x, y + 1);
-
 	for(int i = 0; i < h; i++)
 	{
 		for (int j = 0; j < w; j++)
@@ -153,16 +174,22 @@ void level2(char *s, int w, int h, FILE *file)
 	
 	getline(&line, &bufsize, file);
 	char *inputs = strdup(line);
-	char **valid_map = dup_map(map, h, w);
+	char **valid_map = NULL;
+	valid_map = dup_map(map, h, w);
 
-	if (is_valid(valid_map, inputs, w, h, 0, 0))
+	if (is_valid(map, inputs, w, h, 0, 0))
 		printf("VALID\n");
 	else
 		printf("INVALID\n");
-	//if (move_pos(map, inputs, 1, 2, h, w))
-	//	printf("VALID\n");
-	//else
-	//	printf("INVALID\n");
+	int m = 0;
+	while (m < h)
+	{
+		free(valid_map[m]);
+		free(map[m++]);
+	}
+	free(map);
+	free(valid_map);
+	free(inputs);
 
 }
 
@@ -178,6 +205,7 @@ int is_space(char *s)
 
 int main ()
 {
+	//FILE *file = fopen("level3_2.in", "r");
 	FILE *file = fopen("result", "r");
 	char *line;
 	size_t bufsize = 1;
